ee7e9bdab17a5ac194816a5173c08dd616f7e1cb
OscarTum <oscar.tumbaco01@epn.edu.ec>
2025-02-08 01:22:02 -0500

    Button consulta

--- ProyectoEMC/static/js/buttonConsulta.js
+++ ProyectoEMC/static/js/buttonConsulta.js
@@ -2,17 +2,31 @@ function setActiveButton(button, id) {
     const buttons = document.querySelectorAll('.custom-button');
     buttons.forEach(btn => btn.classList.remove('active'));
     button.classList.add('active');
-    localStorage.setItem('activeButton', id);
+
+    const pageKey = `activeButton_${window.location.pathname}`;
+    localStorage.setItem(pageKey, id);
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const activeButtonId = localStorage.getItem('activeButton');
-    if (activeButtonId) {
-        const activeButton = document.getElementById(activeButtonId);
-        if (activeButton) {
-            activeButton.classList.add('active');
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.custom-button');
+    buttons.forEach(btn => btn.classList.remove('active'));
+
+    const pageKey = `activeButton_${window.location.pathname}`;
+    let activeButtonId = localStorage.getItem(pageKey);
+
+    if (!activeButtonId) {
+        activeButtonId = 'boton2'; 
+        localStorage.setItem(pageKey, 'boton2');
+    }
+
+    const activeButton = document.getElementById(activeButtonId);
+    if (activeButton) {
+        activeButton.classList.add('active');
+
+        const formId = `form-${activeButtonId}`;
+        const form = document.getElementById(formId);
+        if (form) {
+            form.submit();
         }
-    } else {
-        document.getElementById('boton2').classList.add('active');
     }
 });
